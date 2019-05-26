04b8c2f8e774a50ef3a9fec10687c428f704beba
xiz7 <xavier_ar7@hotmail.com>
2019-05-26 23:11:17 +0100

    mmmm

--- site/sqlite.sql
+++ site/sqlite.sql
@@ -10,3 +10,6 @@ CREATE TABLE examples(
 INSERT INTO examples(imageName, stars, dateAdded, imageurl) VALUES("America", 10, strftime('%s','now'), "/images/america.jpg");
 INSERT INTO examples(imageName, stars, dateAdded, imageurl) VALUES("European", 20, strftime('%s','2014-01-01 02:34:56'), "/images/european.jpg");
 INSERT INTO examples(imageName, stars, dateAdded, imageurl) VALUES("Farmhouse", 30, strftime('%s','2015-03-05 15:34:23'), "/images/farmhouse.png");
+INSERT INTO examples(imageName, stars, dateAdded, imageurl) VALUES("SVGAmerica", 99, strftime('%s','now'), "/images/america.svg");
+INSERT INTO examples(imageName, stars, dateAdded, imageurl) VALUES("SuperEuropean", 20, strftime('%s','2015-01-01 04:24:46'), "/images/european.svg");
+INSERT INTO examples(imageName, stars, dateAdded, imageurl) VALUES("SVGFarmhouse", 35, strftime('%s','2018-03-07 05:14:13'), "/images/farmhouse.svg");
